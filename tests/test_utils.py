b253ca3451409a05a1be567b6b938f904e953dae
George Zubrienko <GZU@ecco.com>
2022-02-18 12:55:52 +0100

    Use monotonic instead

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,8 +6,8 @@ from proteus.utils import doze
 
 @pytest.mark.parametrize('sleep_period,doze_interval', [(1, 50), (2, 10)])
 def test_doze(sleep_period: int, doze_interval: int):
-    start_ts = time.process_time_ns() / 1e9
+    start_ts = time.monotonic()
     doze(sleep_period, doze_interval)
-    time_passed = time.process_time_ns() / 1e9 - start_ts
+    time_passed = time.monotonic() - start_ts
 
     assert int(time_passed) == sleep_period
