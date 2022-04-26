7d5eecd8615b1a7f2ac352913e3079fbb13ad214
Matthias Als <mata@ecco.com>
2022-04-26 13:34:02 +0200

    Issue #35, ServiceBusConnector and adds disposal methods to CrystalConnector (#43)
    Issue #35, ServiceBusConnector and adds disposal methods to CrystalConnector

---
+++ proteus/connectors/service_bus/_connector.py
@@ -0,0 +1,40 @@
+"""
+    Connector for Azure Service Bus.
+"""
+from typing import Optional
+import os
+from azure.servicebus import ServiceBusSender, ServiceBusClient, TransportType, ServiceBusMessage
+
+
+class AzureServiceBusConnector:
+    """
+    Connector for Azure Service Bus.
+    """
+    def __init__(self, conn_str: Optional[str] = None, queue_name: Optional[str] = None):
+        self.service_bus_client: ServiceBusClient = ServiceBusClient.from_connection_string(
+            conn_str=conn_str if conn_str is not None else os.environ['SERVICE_BUS_CONNECTION_STRING'],
+            transport_type=TransportType.Amqp,
+        )
+        self.sender: ServiceBusSender = self.service_bus_client.get_queue_sender(
+            queue_name=queue_name if queue_name is not None else os.environ['SERVICE_BUS_QUEUE']
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.dispose()
+
+    def send_message(self, message: str) -> None:
+        """
+        Send string message to service bus
+        """
+        sb_message = ServiceBusMessage(message)
+        self.sender.send_messages(sb_message)
+
+    def dispose(self) -> None:
+        """
+        Gracefully dispose object.
+        """
+        self.sender.close()
+        self.service_bus_client.close()
