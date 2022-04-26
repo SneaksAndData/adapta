77a829b7b21187cd5d1f4fd609ec31b1929a2838
Matthias Als <mata@ecco.com>
2022-04-26 11:28:56 +0200

    Issue #32 - function to save df as blob

--- proteus/storage/blob/base.py
+++ proteus/storage/blob/base.py
@@ -2,10 +2,13 @@
  Abstraction for storage operations.
 """
 from abc import ABC, abstractmethod
-from typing import Optional, Dict
+from typing import Optional, Dict, Type
+
+import pandas as pd
 
 from proteus.security.clients import ProteusClient
 from proteus.storage.models.base import DataPath
+from proteus.storage.models.format import DataframeSerializationFormat, ParquetSerializationFormat
 
 
 class StorageClient(ABC):
@@ -16,7 +19,13 @@ class StorageClient(ABC):
         self._base_client = base_client
 
     @abstractmethod
-    def save_bytes_as_blob(self, data_bytes: bytes, blob_path: DataPath, metadata: Optional[Dict[str, str]] = None, overwrite = False):
+    def save_bytes_as_blob(
+        self,
+        data_bytes: bytes,
+        blob_path: DataPath,
+        metadata: Optional[Dict[str, str]] = None,
+        overwrite: bool = False
+    ) -> None:
         """
          Saves byte array to a blob.
 
@@ -28,7 +37,7 @@ class StorageClient(ABC):
         """
 
     @abstractmethod
-    def get_blob_uri(self, blob_path: DataPath, **kwargs):
+    def get_blob_uri(self, blob_path: DataPath, **kwargs) -> str:
         """
          Generates a URL which can be used to download this blob.
 
@@ -36,3 +45,29 @@ class StorageClient(ABC):
         :param kwargs:
         :return:
         """
+
+    def save_df_as_blob(
+        self,
+        df: pd.DataFrame,
+        blob_path: DataPath,
+        metadata: Optional[Dict[str, str]] = None,
+        overwrite: bool = False,
+        format_: Type[DataframeSerializationFormat] = ParquetSerializationFormat
+    ) -> None:
+        """
+         Saves dataframe with the given serialization format.
+
+        :param df: Dataframe to save.
+        :param blob_path: Blob path in DataPath notation.
+        :param metadata: Optional blob tags or metadata to attach.
+        :param overwrite: whether a blob should be overwritten or an exception thrown if it already exists.
+        :param format_: The serialization format.
+        :return:
+        """
+        bytes_ = format_().serialize(df)
+        self.save_bytes_as_blob(
+            blob_path=blob_path,
+            metadata=metadata,
+            overwrite=overwrite,
+            data_bytes=bytes_
+        )
---
+++ proteus/storage/models/format.py
@@ -0,0 +1,46 @@
+"""
+Serialization formats for saving data structures as blob.
+"""
+from abc import ABC, abstractmethod
+import pandas as pd
+from proteus.storage.blob._functions import json_to_bytes
+
+
+class DataframeSerializationFormat(ABC):
+    """
+    Abstract dataframe serialization format.
+    """
+    @abstractmethod
+    def serialize(self, df: pd.DataFrame) -> bytes:
+        """
+        Serializes dataframe to bytes given a format.
+        :param df: Dataframe to serialize.
+        :return: Serialized dataframe as byte array.
+        """
+        pass
+
+
+class ParquetSerializationFormat(DataframeSerializationFormat):
+    """
+    Serializes dataframes as parquet format.
+    """
+    def serialize(self, df: pd.DataFrame) -> bytes:
+        """
+        Serializes dataframe to bytes using parquet format.
+        :param df: Dataframe to serialize.
+        :return: Parquet serialized dataframe as byte array.
+        """
+        return df.to_parquet()
+
+
+class JsonSerializationFormat(DataframeSerializationFormat):
+    """
+    Serializes dataframes as JSON format.
+    """
+    def serialize(self, df: pd.DataFrame) -> bytes:
+        """
+        Serializes dataframe to bytes using JSON format.
+        :param df: Dataframe to serialize.
+        :return: JSON serialized dataframe as byte array.
+        """
+        return json_to_bytes(df.to_dict(orient='records'))
