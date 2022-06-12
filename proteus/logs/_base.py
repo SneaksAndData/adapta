019d05fa3b69f995cdee29d33a1db3eed2d4077f
George Zubrienko <zubrienkog@gmail.com>
2022-06-12 09:45:36 +0200

    Use default stack level (#72)

--- proteus/logs/_base.py
+++ proteus/logs/_base.py
@@ -77,6 +77,8 @@ class ProteusLogger:
         if diagnostics:
             base_object.setdefault('diagnostics', diagnostics)
 
+        base_object.update(**kwargs)
+
         return json.dumps(base_object)
 
     def _get_logger(self, log_source_name: Optional[str] = None) -> logging.Logger:
@@ -120,7 +122,7 @@ class ProteusLogger:
         """
         logger = self._get_logger(log_source_name)
         logger.warning(msg=self._prepare_message(template=template, tags=tags, diagnostics=None, **kwargs),
-                       exc_info=exception, stack_info=True, stacklevel=3)
+                       exc_info=exception, stack_info=True)
 
     def error(self, template: str, exception: BaseException, tags: Optional[str] = None,
               log_source_name: Optional[str] = None, **kwargs) -> None:
@@ -136,7 +138,7 @@ class ProteusLogger:
         """
         logger = self._get_logger(log_source_name)
         logger.error(msg=self._prepare_message(template=template, tags=tags, diagnostics=None, **kwargs),
-                     exc_info=exception, stack_info=True, stacklevel=3)
+                     exc_info=exception, stack_info=True)
 
     def debug(self, template: str, exception: BaseException, diagnostics: Optional[str] = None,  # pylint: disable=R0913
               tags: Optional[str] = None,
@@ -154,4 +156,4 @@ class ProteusLogger:
         """
         logger = self._get_logger(log_source_name)
         logger.error(msg=self._prepare_message(template=template, tags=tags, diagnostics=diagnostics, **kwargs),
-                     exc_info=exception, stack_info=True, stacklevel=3)
+                     exc_info=exception, stack_info=True)
