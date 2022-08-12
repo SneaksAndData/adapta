3fb549feb326fbd0d5245122eb4654a19ca8bb36
George Zubrienko <GZU@ecco.com>
2022-08-12 13:26:20 +0200

    Support hive entities outside spark context

---
+++ proteus/storage/models/hive.py
@@ -0,0 +1,84 @@
+"""
+ Models used by Hive storages.
+"""
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+from proteus.logs import ProteusLogger
+from proteus.storage.models.base import DataPath, DataProtocols
+from storage.database.azure_sql import AzureSqlClient
+
+
+@dataclass
+class HivePath(DataPath):
+    """
+     Path wrapper for ADLS Gen2.
+    """
+    hive_server: str
+    hive_server_port: str
+    hive_database: str
+    hive_schema: str
+    hive_engine: str
+    path: str
+    protocol: str = DataProtocols.HIVE.value
+    hive_table: Optional[str] = None
+
+    @classmethod
+    def from_hdfs_path(cls, hdfs_path: str) -> "HivePath":
+        # sample path
+        # hive://engine@my-hive-server.net:1234/database/schema/table
+        assert '@' in hdfs_path and hdfs_path.startswith(
+            'hive://'), 'Invalid Hive path supplied. Please use the following format: hive://<engine>@<server address>:<server port>/database/schema/table'
+
+        return HivePath(
+            hive_server=hdfs_path.split('@')[1].split('/')[0].split(':')[0],
+            hive_server_port=hdfs_path.split('@')[1].split('/')[0].split(':')[1],
+            hive_database=hdfs_path.split('@')[1].split('/')[1],
+            hive_schema=hdfs_path.split('@')[1].split('/')[1],
+            hive_table=hdfs_path.split('@')[1].split('/')[2],
+            hive_engine=hdfs_path.split('@')[0].split('//')[1],
+            path='/'.join(hdfs_path.split('@')[1].split('/')[1:])
+        )
+
+    def _check_path(self):
+        assert not self.path.startswith('/'), 'Path should not start with /'
+
+    def to_hdfs_path(self) -> str:
+        self._check_path()
+        return f"{self.protocol}://{self.hive_engine}@{self.hive_server}:{self.hive_server_port}/{self.path}"
+
+    def get_physical_path(self, logger: ProteusLogger) -> Optional[str]:
+        """
+         Converts this virtual HivePath to a physical HDFS path.
+
+        :param logger: Proteus logger for SQL client.
+        :return: Valid HDFS path for the hive entity.
+        """
+        if self.hive_engine == 'sqlserver':
+            with AzureSqlClient(
+                logger=logger,
+                host_name=self.hive_server,
+                port=int(self.hive_server_port),
+                database=self.hive_database,
+                user_name=os.environ['PROTEUS__HIVE_USER'],
+                password=os.environ['PROTEUS__HIVE_PASSWORD']
+            ) as hive_db_client:
+                db_info = hive_db_client.query(f"select * from DBS where name = '{self.hive_schema}'").to_dict()
+                db_id, db_location = db_info['DB_ID'], db_info['DB_LOCATION_URI']
+
+                tbl_info = hive_db_client.query(f"select * from TBLS where db_id = {db_id} and TBL_NAME = '{self.hive_table}'").to_dict()
+                tbl_id, tbl_type = tbl_info['TBL_ID'], tbl_info['TBL_TYPE']
+
+                if tbl_type == 'EXTERNAL':
+                    return hive_db_client.query(f"select * from SERDE_PARAMS where SERDE_ID = {tbl_id} and PARAM_KEY = 'path'").to_dict()['PARAM_VALUE']
+
+                if tbl_type == 'MANAGED_TABLE':
+                    return f"{db_location}/{self.hive_table}"
+
+                return None
+
+        raise NotImplementedError('Engine {engine} is not supported.', self.hive_engine)
+
+    def to_delta_rs_path(self) -> str:
+        raise NotImplementedError
