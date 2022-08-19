b45ff270701a3c91d95b23ef54cb301eea6a0c62
Jeppe Johan Waarkjær Olsen <jjol@ecco.com>
2022-08-19 14:26:09 +0200

    fix test

--- tests/test_delta_history.py
+++ tests/test_delta_history.py
@@ -22,5 +22,12 @@ def test_delta_history(get_client_and_path):
     transactions = list(history(client, data_path))
     operations = [tran.operation.value for tran in transactions]
 
+    assert len(transactions) == 1 and operations == [DeltaOperation.UPDATE.value]
+
+def test_delta_history_full(get_client_and_path):
+    client, data_path = get_client_and_path
+    transactions = list(history(client, data_path, limit=None))
+    operations = [tran.operation.value for tran in transactions]
+
     assert len(transactions) == 3 and operations == [DeltaOperation.CREATE_TABLE_AS_SELECT.value,
                                                      DeltaOperation.WRITE.value, DeltaOperation.UPDATE.value]
