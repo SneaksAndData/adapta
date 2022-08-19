31ea6a173fd4eb9fdaecc94d8be264f3a0f5d3b3
Jeppe Johan Waarkjær Olsen <jeppe742@gmail.com>
2022-08-19 14:38:16 +0200

    limit delta history to latest transaction (#137)

--- tests/test_delta_history.py
+++ tests/test_delta_history.py
@@ -22,5 +22,19 @@ def test_delta_history(get_client_and_path):
     transactions = list(history(client, data_path))
     operations = [tran.operation.value for tran in transactions]
 
+    assert len(transactions) == 1 and operations == [DeltaOperation.UPDATE.value]
+
+def test_delta_history_2(get_client_and_path):
+    client, data_path = get_client_and_path
+    transactions = list(history(client, data_path, limit=2))
+    operations = [tran.operation.value for tran in transactions]
+
+    assert len(transactions) == 2 and operations == [DeltaOperation.WRITE.value, DeltaOperation.UPDATE.value]
+
+def test_delta_history_full(get_client_and_path):
+    client, data_path = get_client_and_path
+    transactions = list(history(client, data_path, limit=None))
+    operations = [tran.operation.value for tran in transactions]
+
     assert len(transactions) == 3 and operations == [DeltaOperation.CREATE_TABLE_AS_SELECT.value,
                                                      DeltaOperation.WRITE.value, DeltaOperation.UPDATE.value]
