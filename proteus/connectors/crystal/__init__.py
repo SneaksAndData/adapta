eece571ab5c3269818b4d61bf5299a1de40198bd
Vitaliy Savitskiy <visa@ecco.com>
2022-08-24 17:26:31 +0200

    Move code from Proteus

--- proteus/connectors/crystal/__init__.py
+++
@@ -1,4 +0,0 @@
-"""init file"""
-
-from proteus.connectors.crystal._connector import CrystalConnector
-from proteus.connectors.crystal._models import AlgorithmRunResult, RequestLifeCycleStage, RequestResult
