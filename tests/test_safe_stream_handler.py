1a9f3cd3fa9015bc2c6c9c50f8cf2576050953fa
Vitaliy Savitskiy <visa@ecco.com>
2022-08-24 16:54:25 +0200

    Add some more tests

--- tests/test_safe_stream_handler.py
+++ tests/test_safe_stream_handler.py
@@ -5,8 +5,9 @@ from proteus.logs.handlers.safe_stream_handler import SafeStreamHandler
 
 def test_does_not_close_stderr():
     ssh = SafeStreamHandler(sys.stderr)
+    stream = ssh.stream
     ssh.close()
-    assert not sys.stderr.closed
+    assert not stream.closed
 
 
 def test_does_not_close_stdout():
@@ -15,3 +16,15 @@ def test_does_not_close_stdout():
     ssh.close()
     assert not sys.stdout.closed
     assert stream.closed
+
+
+def test_replace_stdout():
+    ssh = SafeStreamHandler(sys.stdout)
+    stream = ssh.stream
+    assert stream is not sys.stdout
+
+
+def test_not_replaces_other_streams():
+    ssh = SafeStreamHandler(sys.stderr)
+    stream = ssh.stream
+    assert stream is sys.stderr
