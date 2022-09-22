b84e4962cbdc7ae5d669229fcd16fa0f3491435b
Vitaliy Savitskiy <visa@ecco.com>
2022-09-22 12:08:21 +0200

    WIP

--- proteus/storage/secrets/hashicorp_vault_client.py
+++ proteus/storage/secrets/hashicorp_vault_client.py
@@ -1,4 +1,4 @@
-from ctypes import Union
+from typing import Union
 
 import hvac
 
@@ -7,12 +7,20 @@ from proteus.storage.secrets import SecretStorageClient
 
 
 class HashicorpSecretStorageClient(SecretStorageClient):
-    def __init__(self, *, base_client: ProteusClient):
+    def __init__(self, *, base_client: ProteusClient, role="readwrite_secrets"):
         super().__init__(base_client=base_client)
         self._base_client = HashicorpVaultClient.from_base_client(self._base_client)
+        self._access_token = self._base_client.get_access_token()
+        self.client = hvac.Client()
+        self._role = role
 
     def read_secret(self, storage_name: str, secret_name: str) -> Union[bytes, str]:
-        pass
+        response = self.client.auth.jwt.jwt_login(
+            role=self._role,
+            jwt=self._access_token
+        )
+        return response
+
 
     def create_secret(self, storage_name: str, secret_name: str, secret_value: str, b64_encode=False) -> None:
         pass
--- tests/test_vault_client.py
+++ tests/test_vault_client.py
@@ -1,7 +1,18 @@
+import pytest
+
 from proteus.security.clients import HashicorpVaultClient
+from proteus.storage.secrets.hashicorp_vault_client import HashicorpSecretStorageClient
 
 
+@pytest.mark.skip("Uses desktop browser to login, should be only run locally")
 def test_get_credentials():
     client = HashicorpVaultClient(HashicorpVaultClient.TEST_VAULT_ADDRESS)
     credentials = client.get_credentials()
+    assert credentials is not None
+
+
+#@pytest.mark.skip("Uses desktop browser to login, should be only run locally")
+def test_read_secret():
+    client = HashicorpSecretStorageClient(base_client=HashicorpVaultClient(HashicorpVaultClient.TEST_VAULT_ADDRESS))
+    credentials = client.read_secret("secret", "test/secret/with/path")
     assert credentials is not None
