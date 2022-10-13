982043df7ae50389737857827cd3783df6e14b8f
Vitaliy Savitskiy <visa@ecco.com>
2022-10-13 17:26:39 +0200

    Fix

--- proteus/security/clients/__init__.py
+++ proteus/security/clients/__init__.py
@@ -8,6 +8,7 @@ try:
     from proteus.security.clients._hashicorp_vault_client import HashicorpVaultClient
 except ImportError:
     pass
+
 try:
     from proteus.security.clients._azure_client import AzureClient
 except ImportError:
