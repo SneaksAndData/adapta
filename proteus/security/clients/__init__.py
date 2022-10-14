e6338f2dfd207c2b0e3fc37d98f2eedd5d272556
Vitaliy Savitskiy <visa@ecco.com>
2022-10-14 10:44:06 +0200

    Not fail on import AzureClient if proteus is installed without extra hashicorp (#164)

--- proteus/security/clients/__init__.py
+++ proteus/security/clients/__init__.py
@@ -2,7 +2,14 @@
  Import index.
 """
 
-from proteus.security.clients._azure_client import AzureClient
 from proteus.security.clients._local_client import LocalClient
 from proteus.security.clients._base import ProteusClient
-from proteus.security.clients._hashicorp_vault_client import HashicorpVaultClient
+try:
+    from proteus.security.clients._hashicorp_vault_client import HashicorpVaultClient
+except ImportError:
+    pass
+
+try:
+    from proteus.security.clients._azure_client import AzureClient
+except ImportError:
+    pass
