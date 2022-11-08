1339c997805f7c2bc7c34a2e91d5176a57e07c2e
Vitaliy Savitskiy <visa@ecco.com>
2022-11-08 14:57:49 +0100

    Integration with external loggers (#170)
    Co-authored-by: George Zubrienko <gzu@ecco.com>

--- proteus/logs/models/__init__.py
+++ proteus/logs/models/__init__.py
@@ -3,3 +3,4 @@
 """
 
 from proteus.logs.models._log_level import LogLevel
+from proteus.logs.models._logs_metadata import ProteusLogMetadata
--- tests/test_logs.py
+++ tests/test_logs.py
@@ -1,21 +1,25 @@
 import json
+import logging
 import os
-import socket
+import sys
 import traceback
 from logging import StreamHandler
 
 import tempfile
 from typing import Dict
+from unittest.mock import patch
 
 import pytest
 import uuid
 
-from datadog_api_client.v2.model.http_log_item import HTTPLogItem
+import requests
 from pytest_mock import MockerFixture
 
-from proteus.logs.handlers.datadog_api_handler import DataDogApiHandler
 from proteus.logs import ProteusLogger
+from proteus.logs.handlers.datadog_api_handler import DataDogApiHandler
 from proteus.logs.models import LogLevel
+
+EXPECTED_MESSAGE = 'This a unit test logger 1, Fixed message1 this is a fixed message1, Fixed message2 this is a fixed message2\n'
 
 
 @pytest.mark.parametrize('level,template,args,exception,diagnostics,expected_message', [
@@ -25,7 +29,7 @@ from proteus.logs.models import LogLevel
             {'index': 1},
             None,
             None,
-            '{"template": "This a unit test logger {index}, Fixed message1 {message1}, Fixed message2 {message2}", "text": "This a unit test logger 1, Fixed message1 this is a fixed message1, Fixed message2 this is a fixed message2", "message1": "this is a fixed message1", "message2": "this is a fixed message2", "index": 1}'
+            EXPECTED_MESSAGE
     ),
     (
             LogLevel.WARN,
@@ -33,7 +37,7 @@ from proteus.logs.models import LogLevel
             {'index': 1},
             ValueError("test warning"),
             None,
-            '{"template": "This a unit test logger {index}, Fixed message1 {message1}, Fixed message2 {message2}", "text": "This a unit test logger 1, Fixed message1 this is a fixed message1, Fixed message2 this is a fixed message2", "message1": "this is a fixed message1", "message2": "this is a fixed message2", "index": 1}'
+            EXPECTED_MESSAGE
     ),
     (
             LogLevel.ERROR,
@@ -41,7 +45,7 @@ from proteus.logs.models import LogLevel
             {'index': 1},
             ValueError("test error"),
             None,
-            '{"template": "This a unit test logger {index}, Fixed message1 {message1}, Fixed message2 {message2}", "text": "This a unit test logger 1, Fixed message1 this is a fixed message1, Fixed message2 this is a fixed message2", "message1": "this is a fixed message1", "message2": "this is a fixed message2", "index": 1}'
+            EXPECTED_MESSAGE
     ),
     (
             LogLevel.DEBUG,
@@ -49,7 +53,7 @@ from proteus.logs.models import LogLevel
             {'index': 1},
             ValueError("test error"),
             'additional debug info',
-            '{"template": "This a unit test logger {index}, Fixed message1 {message1}, Fixed message2 {message2}", "text": "This a unit test logger 1, Fixed message1 this is a fixed message1, Fixed message2 this is a fixed message2", "diagnostics": "additional debug info", "message1": "this is a fixed message1", "message2": "this is a fixed message2", "index": 1}'
+            EXPECTED_MESSAGE
     )
 ])
 def test_log_format(level: LogLevel, template: str, args: Dict, exception: BaseException, diagnostics: str,
@@ -77,11 +81,7 @@ def test_log_format(level: LogLevel, template: str, args: Dict, exception: BaseE
             stream_logger.debug(template=template, exception=exception, diagnostics=diagnostics, **args)
 
     logged_lines = open(test_file_path, 'r').readlines()
-    try:
-        entry = json.loads(''.join(logged_lines))
-        assert entry['message'] == expected_message
-    except Exception as ex:
-        raise ex
+    assert expected_message in logged_lines
 
 
 def test_datadog_api_handler(mocker: MockerFixture):
@@ -104,10 +104,84 @@ def test_datadog_api_handler(mocker: MockerFixture):
         dd_logger.warning(template='This a unit test logger {index}', exception=ex, index=1)
         ex_str = traceback.format_exc().removesuffix("\n")
 
-    assert mock_handler._buffer[0] == HTTPLogItem(
-        ddsource=mock_source,
-        ddtags='environment:local',
-        hostname=socket.gethostname(),
-        message=json.dumps({"template": "This a unit test logger {index}", "text": "This a unit test logger 1", "index": 1, "error": {"stack": ex_str, "message": None, ''"kind": "ValueError"}}),
-        status='WARNING'
-    )
+    log_item = mock_handler._buffer[0]
+    message = json.loads(log_item.message)
+
+    assert log_item.ddsource == mock_source
+    assert log_item.ddtags == 'environment:local'
+    assert log_item.status == 'WARNING'
+    assert message["text"] == "This a unit test logger 1"
+    assert message["index"] == 1
+    assert message["error"] == {"stack": ex_str, "message": "test warning", "kind": "ValueError"}
+    assert message["template"] == "This a unit test logger {index}"
+    assert "tags" not in message
+
+
+def test_proteus_logger_replacement(mocker: MockerFixture, restore_logger_class):
+    mocker.patch('proteus.logs.handlers.datadog_api_handler.DataDogApiHandler._flush', return_value=None)
+
+    mock_environment = {
+        'PROTEUS__DD_API_KEY': 'some-key',
+        'PROTEUS__DD_APP_KEY': 'some-app-key',
+        'PROTEUS__DD_SITE': 'some-site.dog'
+    }
+    with patch.dict(os.environ, mock_environment):
+        ProteusLogger().add_log_source(log_source_name="urllib3",
+                                       min_log_level=LogLevel.DEBUG,
+                                       log_handlers=[DataDogApiHandler()])
+        requests.get("https://example.com")
+
+    requests_log = logging.getLogger("urllib3")
+    handler = [handler for handler in requests_log.handlers if isinstance(handler, DataDogApiHandler)][0]
+    buffers = [json.loads(msg.message) for msg in handler._buffer]
+    assert {'text': 'Starting new HTTPS connection (1): example.com:443'} in buffers
+
+
+def test_log_level(mocker: MockerFixture, restore_logger_class):
+    mocker.patch('proteus.logs.handlers.datadog_api_handler.DataDogApiHandler._flush', return_value=None)
+
+    mock_environment = {
+        'PROTEUS__DD_API_KEY': 'some-key',
+        'PROTEUS__DD_APP_KEY': 'some-app-key',
+        'PROTEUS__DD_SITE': 'some-site.dog'
+    }
+    with patch.dict(os.environ, mock_environment):
+        logger = ProteusLogger() \
+            .add_log_source(log_source_name="test",
+                            min_log_level=LogLevel.INFO,
+                            log_handlers=[DataDogApiHandler()])
+        logger.debug("Debug message", log_source_name="test")
+        logger.info("Info message", log_source_name="test")
+
+    requests_log = logging.getLogger("test")
+    handler = [handler for handler in requests_log.handlers if isinstance(handler, DataDogApiHandler)][0]
+    buffers = [json.loads(msg.message) for msg in handler._buffer]
+    assert buffers == [{'template': 'Info message', 'text': 'Info message'}]
+
+
+def test_fixed_template(mocker: MockerFixture, restore_logger_class):
+    mocker.patch('proteus.logs.handlers.datadog_api_handler.DataDogApiHandler._flush', return_value=None)
+
+    mock_environment = {
+        'PROTEUS__DD_API_KEY': 'some-key',
+        'PROTEUS__DD_APP_KEY': 'some-app-key',
+        'PROTEUS__DD_SITE': 'some-site.dog'
+    }
+    with patch.dict(os.environ, mock_environment):
+        logger = ProteusLogger(
+            fixed_template={'running with job id {job_id} on {owner}': {'job_id': 'my_job_id', 'owner': 'owner'}},
+            fixed_template_delimiter='|'
+        ) \
+            .add_log_source(log_source_name="test_fixed_template",
+                            min_log_level=LogLevel.INFO,
+                            log_handlers=[DataDogApiHandler()])
+        logger.info("Custom template={custom_value}", log_source_name="test_fixed_template", custom_value="my-value")
+
+    requests_log = logging.getLogger("test_fixed_template")
+    handler = [handler for handler in requests_log.handlers if isinstance(handler, DataDogApiHandler)][0]
+    buffers = [json.loads(msg.message) for msg in handler._buffer]
+    assert buffers == [{'template': 'Custom template={custom_value}|running with job id {job_id} on {owner}',
+                        'custom_value': 'my-value',
+                        'job_id': 'my_job_id',
+                        'owner': 'owner',
+                        'text': 'Custom template=my-value|running with job id my_job_id on owner'}]
