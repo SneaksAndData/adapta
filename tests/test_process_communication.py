687ac7ad32d742dbbc526635b692610377248806
George Zubrienko <GZU@ecco.com>
2022-12-08 11:57:21 +0100

    Add json parse

--- tests/test_process_communication.py
+++ tests/test_process_communication.py
@@ -1,4 +1,4 @@
-from typing import Optional, Iterable
+from typing import Optional, Iterable, Union, Dict
 
 import pytest
 
@@ -30,6 +30,37 @@ def test_data_socket_serialize(alias: str, data_path: str, data_format: str, exp
 @pytest.mark.parametrize(
     'value,expected_deser',
     [
+        (
+                {
+                    "alias": "test",
+                    "data_path": "abfss://some-data@azureaccount.dfs.core.windows.net/some_folder/some_table",
+                    "data_format": "delta"
+                },
+                DataSocket(
+                    "test",
+                    "abfss://some-data@azureaccount.dfs.core.windows.net/some_folder/some_table",
+                    "delta"
+                )
+        ),
+        (
+                '{"alias": "test","data_path": "abfss://some-data@azureaccount.dfs.core.windows.net/some_folder/some_table","data_format": "delta"}',
+                DataSocket(
+                    "test",
+                    "abfss://some-data@azureaccount.dfs.core.windows.net/some_folder/some_table",
+                    "delta"
+                )
+        )
+    ]
+)
+def test_socket_from_json(value: Union[str, Dict], expected_deser: Optional[DataSocket]):
+    test_socket = DataSocket.from_json(value) if type(value) is str else DataSocket.from_dict(value)
+
+    assert test_socket == expected_deser
+
+
+@pytest.mark.parametrize(
+    'value,expected_deser',
+    [
         ("test|file://some-folder/here|jpg", DataSocket("test", "file://some-folder/here", "jpg")),
         ("test|file://some-folder/here|", None),
     ]
@@ -47,7 +78,8 @@ def test_data_socket_deserialize(value: str, expected_deser: Optional[DataSocket
     'alias,data_path,data_format,expected_path',
     [
         ("test", "file://some-folder/file", "text", LocalPath(path='some-folder/file')),
-        ("test", "abfss://container@account.dfs.core.windows.net/some-folder/file", "text", AdlsGen2Path.from_hdfs_path("abfss://container@account.dfs.core.windows.net/some-folder/file")),
+        ("test", "abfss://container@account.dfs.core.windows.net/some-folder/file", "text",
+         AdlsGen2Path.from_hdfs_path("abfss://container@account.dfs.core.windows.net/some-folder/file")),
         ("test", "api://some-folder/file", "json", None),
     ]
 )
