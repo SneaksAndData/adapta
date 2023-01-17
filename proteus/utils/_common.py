ddae5840ab420e945d5335298e6730d5767e5dc0
George Zubrienko <GZU@ecco.com>
2023-01-17 21:19:14 +0100

    Enabled parallelisation of function execution

--- proteus/utils/_common.py
+++ proteus/utils/_common.py
@@ -1,10 +1,14 @@
 """Common utility functions. All of these are imported into __init__.py"""
+import concurrent.futures
 import contextlib
 import math
+import os
+import sys
 import time
 from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from functools import partial
-from typing import List, Optional, Dict, Any
+from typing import List, Optional, Dict, Any, Callable, Tuple
 
 import requests
 from requests.adapters import HTTPAdapter
@@ -91,3 +95,34 @@ def chunk_list(value: List[Any], num_chunks: int) -> List[List[Any]]:
     """
     chunk_size = math.ceil(len(value) / num_chunks)
     return [value[el_pos:el_pos + chunk_size] for el_pos in range(0, len(value), chunk_size)]
+
+
+def parallelise(
+        func_list: List[Tuple[Callable[[...], Any], List[Any], str]],
+        num_threads: Optional[int] = None,
+        use_processes: bool = False
+) -> Dict[str, concurrent.futures.Future]:
+    """
+     Lazily parallelises execution of a list of functions. Usage example:
+
+     my_funcs = [(my_func1, [arg1, .., argN], 'task1'),..]
+     threads = len(os.sched_getaffinity(0))
+     tasks = parallelise(func_list, threads, True)
+
+     task_n_result = tasks['taskN'].result()
+
+     For I/O bound work, set use_processes to True, otherwise overall run time will not improve due to GIL.
+
+    :param func_list: A list of (function, arguments) tuples to parallelise.
+    :param num_threads: Maximum number of threads to use. On Linux platforms use len(os.sched_getaffinity(0))
+      to get number of threads available to current process
+    :param use_processes: Use processes instead of thread for parallelisation. Preferrable for work that depends on GIL release.
+    :return: A dictionary of (callable_name, callable_future)
+    """
+    worker_count = num_threads or (len(os.sched_getaffinity(0)) if sys.platform != "win32" else os.cpu_count())
+    with ThreadPoolExecutor(max_workers=worker_count) \
+            if not use_processes \
+            else ProcessPoolExecutor(max_workers=worker_count) as runner_pool:
+        return {
+            func_alias: runner_pool.submit(func, *func_args) for func, func_args, func_alias in func_list
+        }
