2a9147118c2809ff369748dcdb78506abecb2dab
George Zubrienko <GZU@ecco.com>
2023-03-06 10:21:11 +0100

    Add copyright for OSS

--- proteus/security/clients/_local_client.py
+++ proteus/security/clients/_local_client.py
@@ -1,6 +1,21 @@
 """
  Client representing Local infrastructure. Mainly used for unit tests.
 """
+#  Copyright (c) 2023. ECCO Sneaks & Data
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 from typing import Optional, Dict
 from pyarrow.fs import FileSystem, LocalFileSystem, SubTreeFileSystem
 from proteus.security.clients._base import ProteusClient
