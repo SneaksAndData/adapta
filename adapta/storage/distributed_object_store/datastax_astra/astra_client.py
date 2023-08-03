73d7e4a1a60003652d3800c37b62a4b3b6762531
George Zubrienko <gzu@ecco.com>
2023-08-03 17:20:58 +0200

    Fix another issue with enum compat (#305)

--- adapta/storage/distributed_object_store/datastax_astra/astra_client.py
+++ adapta/storage/distributed_object_store/datastax_astra/astra_client.py
@@ -300,8 +300,12 @@ class AstraClient:
                 return (columns.Integer,)
             if python_type is float:
                 return (columns.Double,)
-            if (sys.version_info.minor > 9 and python_type is enum.EnumType) or (
-                sys.version_info.minor <= 9 and python_type is enum.EnumMeta
+            if (
+                sys.version_info.minor > 9
+                and type(python_type) is enum.EnumType  # pylint: disable=unidiomatic-typecheck
+            ) or (
+                sys.version_info.minor <= 9
+                and type(python_type) is enum.EnumMeta  # pylint: disable=unidiomatic-typecheck
             ):  # assume all enums are strings - for now
                 return (columns.Text,)
             if get_origin(python_type) == list:
