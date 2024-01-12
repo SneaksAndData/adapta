b6e1f7fc69b91a312a83ed2845869fbbc0270b07
George Zubrienko <gzu@ecco.com>
2024-01-12 13:22:54 +0100

    Fix missing import (#386)

--- adapta/utils/decorators/_logging.py
+++ adapta/utils/decorators/_logging.py
@@ -1,6 +1,6 @@
 """ Module for common decorator methods. """
 from functools import wraps
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 
 from adapta.logs import SemanticLogger
 from adapta.logs._async_logger import _AsyncLogger
@@ -55,12 +55,19 @@ def run_time_metrics(metric_name: str, tag_function_name: bool = False, log_leve
     return outer_runtime_decorator
 
 
-def run_time_metrics_async(metric_name: str, tag_function_name: bool = False):
+def run_time_metrics_async(
+    metric_name: str,
+    tag_function_name: bool = False,
+    on_finish_message_template="Method {method_name} finished in {elapsed:.2f}s seconds",
+    template_args: Optional[dict[str, Any]] = None,
+):
     """
     Decorator that records runtime of decorated method to logging source and metrics_provider.
 
     :param metric_name: Description of method type that can be used to capture logging in metric sink.
     :param tag_function_name: Boolean flag to indicate if function name should be added as tag to metric. Default False.
+    :param on_finish_message_template: Message to log on INFO level when run is finished. Must contain `{elapsed}` in order to log the time correctly.
+    :param template_args: Template arguments, without {elapsed}.
     """
 
     def outer(func):
@@ -73,7 +80,7 @@ def run_time_metrics_async(metric_name: str, tag_function_name: bool = False):
         ):
             metric_tags = (metric_tags or {}) | ({"function_name": str(func.__name__)} if tag_function_name else {})
 
-            logger.debug("Running {run_type} on method {method_name}", run_type=metric_name, method_name=func.__name__)
+            logger.debug("Running {method_name}", method_name=func.__name__)
 
             with operation_time() as ot:
                 result = await func(metrics_provider=metrics_provider, logger=logger, metric_tags=metric_tags, **kwargs)
@@ -83,11 +90,12 @@ def run_time_metrics_async(metric_name: str, tag_function_name: bool = False):
                 metric_value=round(ot.elapsed / 1e9, 2),
                 tags=metric_tags,
             )
+            nonlocal template_args
+            template_args = (template_args or {"method_name": func.__name__}) | {"elapsed": ot.elapsed / 1e9}
 
-            logger.debug(
-                "Method {method_name} finished in {elapsed:.2f}s seconds",
-                method_name=func.__name__,
-                elapsed=(ot.elapsed / 1e9),
+            logger.info(
+                on_finish_message_template,
+                **template_args,
             )
             return result
 
