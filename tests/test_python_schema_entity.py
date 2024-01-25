299a8c03982640e475c64ff1f8d92df9a2917b9a
hfj <henrikfoss_21@hotmail.com>
2024-01-25 15:17:26 +0100

    added tests

--- tests/test_python_schema_entity.py
+++ tests/test_python_schema_entity.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import pytest
 
@@ -12,14 +12,45 @@ class TestSchema:
     col_c: int
     col_d: object
 
+
+@dataclass
+class ReviewTime:
+    location_key: str = field(
+        metadata={
+            "display_name": "location Key",
+            "description": "Unique identifier for location.",
+            "is_primary_key": True,
+            "is_partition_key": True,
+        }
+    )
+
+    sku_key: str = field(
+        metadata={
+            "display_name": "sku key",
+            "description": "Unique identifier of a sku_key",
+            "is_primary_key": True,
+            "is_partition_key": True,
+        }
+    )
+
+    review_time: int = field(
+        metadata={
+            "display_name": "Review time",
+            "description": "Review time found from auto replenishment.",
+        }
+    )
+
+
 @pytest.mark.parametrize(
     "SCHEMA, columns",
     [
         PythonSchemaEntity(TestSchema),
         ["col_a", "col_b", "col_c", "col_d"],
     ],
+    [
+        PythonSchemaEntity(ReviewTime),
+        ["location_key", "sku_key", "review_time"],
+    ],
 )
-def test_get_field_names(
-    SCHEMA, columns: list[str]
-):
+def test_get_field_names(SCHEMA, columns: list[str]):
     assert SCHEMA.get_field_names() == columns
