f889a1c6aa6db0877cef5e04e2f9eabcb158a1a8
George Zubrienko <gzu@ecco.com>
2024-03-07 17:50:13 +0100

    Add support for parsing model class out of Astra paths (#397)

--- adapta/storage/models/astra.py
+++ adapta/storage/models/astra.py
@@ -1,6 +1,7 @@
 """
  Models used by Astra DB when working with storage.
 """
+
 #  Copyright (c) 2023-2024. ECCO Sneaks & Data
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,10 +17,10 @@
 #  limitations under the License.
 #
 
+import re
 from dataclasses import dataclass
 from pydoc import locate
 from typing import Optional, TypeVar, Type
-from urllib.parse import urlparse
 
 from adapta.storage.models.base import DataPath, DataProtocols
 
@@ -49,12 +50,15 @@ class AstraPath(DataPath):
 
     @classmethod
     def from_hdfs_path(cls, hdfs_path: str) -> "AstraPath":
-        assert hdfs_path.startswith("astra://"), "HDFS astra path should start with astra://"
-        parsed_path = urlparse(hdfs_path).netloc.split("@")
-        return cls(keyspace=parsed_path[0], table=parsed_path[1])
+        path_regex = r"^(astra)(?:\+(.+))?:\/\/(.+)@(.+)$"
+        match = re.match(path_regex, hdfs_path)
+        assert match, f"Invalid path provided, must comply with: {path_regex}"
+        return cls(keyspace=match.group(3), table=match.group(4), model_class_name=match.group(2))
 
     def to_hdfs_path(self) -> str:
-        return f"astra://{self.keyspace}@{self.table}"
+        if not self.model_class_name:
+            return f"astra://{self.keyspace}@{self.table}"
+        return f"astra+{self.model_class_name}://{self.keyspace}@{self.table}"
 
     def to_delta_rs_path(self) -> str:
         raise NotImplementedError
