e32568f58515934c0c8354b56bd1fadc68d8bf53
mata <mata@ecco.com>
2024-04-03 09:55:37 +0200

    Fixes test

--- adapta/utils/python_typing/_functions.py
+++ adapta/utils/python_typing/_functions.py
@@ -1,10 +1,14 @@
 """Common python typing functions. All of these are imported into __init__.py"""
 import sys
-from types import UnionType
 from typing import Type, get_origin, Union, get_args
 
+type_hint = Type
+if sys.version_info >= (3, 10):
+    from types import UnionType
+    type_hint = Union[UnionType, Union]
 
-def is_optional(type_: Union[Type, UnionType]) -> bool:
+
+def is_optional(type_: type_hint) -> bool:
     """
     Checks if a type is Optional.
 
@@ -14,6 +18,7 @@ def is_optional(type_: Union[Type, UnionType]) -> bool:
     origin_type = get_origin(type_)
 
     if sys.version_info >= (3, 10):
+        from types import UnionType
         return (origin_type is UnionType or origin_type is Union) and type(None) in get_args(type_)
 
     return origin_type is Union and type(None) in get_args(type_)
