0b5809de7e4e6527ea9245c490bda992300a5636
Matthias Als <matt035343@gmail.com>
2024-04-03 10:44:34 +0200

    Fixes is_optional (#407)

--- adapta/utils/python_typing/_functions.py
+++ adapta/utils/python_typing/_functions.py
@@ -1,13 +1,24 @@
 """Common python typing functions. All of these are imported into __init__.py"""
-
+import sys
 from typing import Type, get_origin, Union, get_args
 
+ArgumentType = Type
+if sys.version_info >= (3, 10):
+    from types import UnionType
 
-def is_optional(type_: Type) -> bool:
+    ArgumentType = Union[UnionType, Type]
+
+
+def is_optional(type_: ArgumentType) -> bool:
     """
     Checks if a type is Optional.
 
     :param type_: Type to check.
     :return: True if the type is Optional, False otherwise.
     """
-    return get_origin(type_) is Union and type(None) in get_args(type_)
+    origin_type = get_origin(type_)
+
+    if sys.version_info >= (3, 10):
+        return (origin_type is UnionType or origin_type is Union) and type(None) in get_args(type_)
+
+    return origin_type is Union and type(None) in get_args(type_)
--- tests/test_python_typing_functions.py
+++ tests/test_python_typing_functions.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Optional, Union, List, Tuple, Any
 
 import pytest
@@ -6,21 +7,33 @@ from adapta.utils.python_typing import is_optional
 
 
 @pytest.mark.parametrize(
-    "test_type,expected",
+    "type_,expected",
     [
-        (is_optional(Optional[Union[str, int]]), True),
-        (is_optional(Optional[str]), True),
-        (is_optional(Union[str, None]), True),  # Same as Optional[str]
-        (is_optional(Union[str, Optional[int]]), True),  # Same as Union[str, int, None], which is an optional type
-        (is_optional(str), False),
-        (is_optional(Union[str, int]), False),
-        (is_optional(List[str]), False),
-        (is_optional(Tuple[int, ...]), False),
+        (Optional[Union[str, int]], True),
+        (Optional[str], True),
+        (Union[str, None], True),  # Same as Optional[str]
+        (Union[str, Optional[int]], True),  # Same as Union[str, int, None], which is an optional type
+        (str, False),
+        (Union[str, int], False),
+        (List[str], False),
+        (Tuple[int, ...], False),
     ],
 )
-def test_is_optional(test_type: Any, expected: bool):
+def test_is_optional(type_: Any, expected: bool):
     """
     Test that the is_optional function correctly identifies optional types.
     """
 
-    assert test_type == expected
+    assert is_optional(type_) == expected
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="Only run this test on Python 3.10+")
+def test_is_optional_python310():
+    """
+    Test that the is_optional function correctly identifies optional types on Python 3.10+.
+
+    str | None is semantically equivalent to Optional[str], but not the same union type
+    """
+
+    assert is_optional(str | None)
+    assert not is_optional(str | int)
