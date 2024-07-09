91015cdefad717920295e9aab72f959d3b905bd0
André Costa <ancosta@gmail.com>
2024-07-09 11:19:23 +0200

    Some more improvements

--- adapta/storage/blob/s3_storage_client.py
+++ adapta/storage/blob/s3_storage_client.py
@@ -88,6 +88,15 @@ class S3StorageClient(StorageClient):
         :return: Boolean indicator of blob existence
         """
         s3_path = cast_path(blob_path)
+
+        if not (hasattr(s3_path, "bucket") and s3_path.bucket) or not (hasattr(s3_path, "path") and s3_path.path):
+            missing_attributes = [
+                attr for attr in ["bucket", "path"] if not (hasattr(s3_path, attr) and getattr(s3_path, attr))
+            ]
+            raise StorageClientError(
+                f"Blob path provided does not have the needed parameters: {', '.join(missing_attributes)}"
+            )
+
         try:
             self._s3_resource.meta.client.head_object(Bucket=s3_path.bucket, Key=s3_path.path)
             return True
@@ -196,8 +205,8 @@ class S3StorageClient(StorageClient):
                 os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
                 try:
                     self._s3_resource.meta.client.download_file(s3_path.bucket, blob.key, local_file_path)
-                except ClientError as error:
-                    raise StorageClientError(f"Error downloading blob: {error}") from error
+                except ClientError:
+                    raise StorageClientError("Error downloading blob")
 
     def copy_blob(self, blob_path: DataPath, target_blob_path: DataPath, doze_period_ms: int = 0) -> None:
         """
