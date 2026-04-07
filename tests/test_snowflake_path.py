9b1dccb94109db0caa682e208663a3d8c952ba5e
ThomasJauho <42926609+ThomasJauho@users.noreply.github.com>
2026-04-07 20:49:01 +0200

    Test: failing test for path

---
+++ tests/test_snowflake_path.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2023-2026. ECCO Data & AI and other project contributors.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+import pytest
+
+from adapta.storage.models.snowflake import SnowflakePath
+
+
+def test_from_hdfs_path():
+    path = SnowflakePath.from_hdfs_path("snowflake://database/schema/table")
+    assert path.database == "database"
+    assert path.schema == "schema"
+    assert path.table == "table"
+
+
+def test_to_hdfs_path():
+    path = SnowflakePath("database", "schema", "table").to_hdfs_path()
+    assert path == "snowflake://database/schema/table"
+
+
+
+
